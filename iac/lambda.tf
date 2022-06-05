837b7c914c95444e9a7dea7a7575553f9fe37da4
Andrew Lai <andrew.s.lai5@gmail.com>
2022-06-05 13:09:16 -0500

    Lambda TF

--- iac/lambda.tf
+++ iac/lambda.tf
@@ -1,5 +1,5 @@
-data "aws_s3_bucket" "andrewslai_bucket" {
-  bucket = "andrewslai"
+data "aws_s3_bucket" "andrewslai_wedding" {
+  bucket = "andrewslai-wedding"
 }
 
 # ----------------------------------------------------------------------------------------------------------------------
@@ -9,8 +9,8 @@ data "aws_s3_bucket" "andrewslai_bucket" {
 
 data "archive_file" "lambda" {
   type        = "zip"
-  source_file = "src/image-processor.py"
-  output_path = "out/image-processor.py.zip"
+  source_file = "../src/image-processor.py"
+  output_path = "../out/image-processor.py.zip"
 }
 
 # Automatic processing when a new photo is uploaded.
@@ -23,11 +23,11 @@ resource "aws_iam_role" "lambda_iam" {
   "Version": "2012-10-17",
   "Statement": [
     {
-      "Effect": "Allow",
+      "Action": "sts:AssumeRole",
       "Principal": {
         "Service": "lambda.amazonaws.com"
       },
-      "Action": "sts:AssumeRole"
+      "Effect": "Allow"
     }
   ]
 }
@@ -44,7 +44,7 @@ resource "aws_iam_role_policy" "s3_access" {
   "Statement": [
     {
       "Action": [
-        "s3:*",
+        "s3:*"
       ],
       "Effect": "Allow",
       "Resource": "*"
@@ -65,9 +65,9 @@ resource "aws_lambda_function" "image_processor" {
 
 # Adding S3 bucket as trigger to my lambda and giving the permissions
 resource "aws_s3_bucket_notification" "aws-lambda-trigger" {
-  bucket = aws_s3_bucket.bucket.id
+  bucket = data.aws_s3_bucket.andrewslai_wedding.id
   lambda_function {
-    lambda_function_arn = aws_lambda_function.test_lambda.arn
+    lambda_function_arn = aws_lambda_function.image_processor.arn
     events              = ["s3:ObjectCreated:*"]
     filter_prefix       = "queued/"
   }
@@ -76,7 +76,7 @@ resource "aws_s3_bucket_notification" "aws-lambda-trigger" {
 resource "aws_lambda_permission" "allow_bucket" {
   statement_id  = "AllowS3Invoke"
   action        = "lambda:InvokeFunction"
-  function_name = aws_lambda_function.image_processer.function_name
+  function_name = aws_lambda_function.image_processor.function_name
   principal     = "s3.amazonaws.com"
-  source_arn    = "arn:aws:s3:::${aws_s3_bucket.bucket.id}"
+  source_arn    = "arn:aws:s3:::${data.aws_s3_bucket.andrewslai_wedding.id}"
 }
